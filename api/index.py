16b949ba34b2c2bf6a046c167dba0d90174a8142
Lilia Amk <120225210+123Lilia@users.noreply.github.com>
2023-12-16 18:25:28 +0100

    Update2

--- api/index.py
+++ api/index.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 import json
 from supabase import create_client, Client
 
 app = Flask(__name__)
+
 url="https://iqacemdedaqxepotxlbb.supabase.co"
 key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxYWNlbWRlZGFxeGVwb3R4bGJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0OTI4OTQsImV4cCI6MjAxODA2ODg5NH0.a8KGVvu2jG9gNlWzi03lMNl7oaIjKZVAf0Qpo6WS5Lk"
 supabase: Client = create_client(url, key)
@@ -13,30 +14,36 @@ def api_users_signup():
     email = request.form.get('email')
     password = request.form.get('password')
     error = False
+
     # Validate name
     if (not name) or (len(name) < 2):  # You can adjust the length requirement
         error = 'Name needs to be valid'
+
     # Validate email
     if (not error) and ((not email) or (len(email) < 5)):  # You can even check with regex
         error = 'Email needs to be valid'
+
     # Validate password
     if (not error) and ((not password) or (len(password) < 5)):
         error = 'Provide a valid password'
+
     # Check if user already exists
     if (not error):
         response = supabase.table('users').select("*").ilike('email', email).execute()
         if len(response.data) > 0:
             error = 'User already exists'
+
     # If no error, proceed with sign-up
     if (not error):
         response = supabase.table('users').insert({"name": name, "email": email, "password": password}).execute()
         print(str(response.data))
         if len(response.data) == 0:
             error = 'Error creating the user'
+
     if error:
         return json.dumps({'status': 200, 'message': error})
-    return json.dumps({'status': 200, 'message': '', 'data': response.data[0]})
 
+    return json.dumps({'status': 200, 'message': '', 'data': response.data[0]})
 @app.route('/users.login',methods=['GET','POST'])
 def api_users_login():
     email= request.form.get('email')
@@ -56,6 +63,7 @@ def api_users_login():
     
     return json.dumps({'status':500,'message':error})        
     
+
 @app.route('/users.signup.auth',methods=['GET','POST'])
 def api_users_signup_auth():
     email= request.args.get('email')
@@ -64,31 +72,45 @@ def api_users_signup_auth():
     print(str(response))    
     return str(response)
 
-@app.route('/users.updateGender', methods=['GET', 'POST'])
-def api_users_update_gender():
+
+
+@app.route('/users.change_password', methods=['POST'])
+def api_users_change_password():
     email = request.form.get('email')
-    gender = request.form.get('gender')
+    old_password = request.form.get('old_password')
+    new_password = request.form.get('new_password')
 
-    # Validate inputs
-    if not (email and gender):
-        return json.dumps({'status': 400, 'message': 'Invalid input'})
+    # Validate email
+    if not email or len(email) < 5:
+        return jsonify({'status': 400, 'message': 'Invalid email'})
 
-    # Get user id from 'users' table
-    user_response = supabase.table('users').select('id').ilike('email', email).execute()
-    if not user_response.data:
-        return json.dumps({'status': 404, 'message': 'User not found'})
+    # Validate old password
+    if not old_password or len(old_password) < 5:
+        return jsonify({'status': 400, 'message': 'Invalid old password'})
 
-    # Get the user's id
-    user_id = user_response.data[0]['id']
+    # Validate new password
+    if not new_password or len(new_password) < 5:
+        return jsonify({'status': 400, 'message': 'Invalid new password'})
 
-    # Insert new row into 'users_info' table
-    result = supabase.table('users_info').insert({'user_id': user_id, 'gender': gender}).execute()
+    # Check if the user exists
+    user_response = supabase.table('users').select("*").ilike('email', email).execute()
+    if len(user_response.data) == 0:
+        return jsonify({'status': 404, 'message': 'User not found'})
 
-    # Check if the gender update was successful
-    if result['status'] == 201:
-        return json.dumps({'status': 200, 'message': 'Gender updated successfully'})
-    else:
-        return json.dumps({'status': result['status'], 'message': result['error']['message']})
+    user_data = user_response.data[0]
+
+    # Check if the old password matches
+    if user_data['password'] != old_password:
+        return jsonify({'status': 401, 'message': 'Incorrect old password'})
+
+    # Update the user's password
+    update_response = supabase.table('users').update({'password': new_password}).eq('email', email).execute()
+
+    if len(update_response.data) == 0:
+        return jsonify({'status': 500, 'message': 'Error updating password'})
+
+    return jsonify({'status': 200, 'message': 'Password updated successfully'})
+
 
 
 @app.route('/')
@@ -96,4 +118,5 @@ def about():
     return 'Welcome '
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5005)
+
