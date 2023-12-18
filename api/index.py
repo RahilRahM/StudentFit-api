c5dbfe197c04b5ac618d2cd27ee3a1077a8a7005
RahilRahM <rahil.rahmouni@ensia.edu.dz>
2023-12-18 03:44:08 +0100

    datetime to JSON format (InsertWeight_Endpoint)

--- api/index.py
+++ api/index.py
@@ -195,7 +195,7 @@ def api_users_insert_height():
 def api_users_insert_weight_record():
     user_id = request.form.get('user_id')
     weight = request.form.get('weight')
-    recorded_at = datetime.now()
+    recorded_at = datetime.now().isoformat()
 
     try:
         if not (user_id and weight):
