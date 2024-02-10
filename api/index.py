3327cb12a9cc45546af660411660660436fd7b00
RahilRahM <rahil.rahmouni@ensia.edu.dz>
2024-02-10 15:53:58 +0100

    api_users_get_user_info

--- api/index.py
+++ api/index.py
@@ -234,8 +234,8 @@ def api_users_get_user_info():
         # Fetch user info from the users_info table
         user_info_response = supabase.table('users_info').select("*").eq('user_id', user_id).execute()
 
-        # Fetch user latest weight from the weight_records table
-        weight_records_response = supabase.table('weight_records').select("weight").eq('user_id', user_id).order('recorded_at', desc=True).limit(1).execute()
+        # Fetch all weight records for the user from the weight_records table
+        weight_records_response = supabase.table('weight_records').select("weight, recorded_at").eq('user_id', user_id).order('recorded_at', desc=True).execute()
 
         if len(user_info_response.data) == 0 and len(weight_records_response.data) == 0:
             return json.dumps({'status': 404, 'message': 'User info not found'})
